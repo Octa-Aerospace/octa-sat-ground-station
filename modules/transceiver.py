b14ee4e86e21950b8cb4c901d3c895992f9bf5ef
CxrlosKenobi <kenobi@ieee.org>
2022-01-16 11:24:09 -0300

    Update transceiver

--- modules/transceiver.py
+++ modules/transceiver.py
@@ -10,8 +10,8 @@ class LORA:
 
         BAUDRATE = 1000000
         RADIO_FREQ_MHZ = 915.0
-        CS = digitalio.DigitalInOut(board.CE1)
-        RESET = digitalio.DigitalInOut(board.D25)
+        CS = digitalio.DigitalInOut(board.CE1) 
+        RESET = digitalio.DigitalInOut(board.D25) # The pin used in the raspberry pi is D25; Number 
         spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
     
         rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=BAUDRATE)
@@ -28,11 +28,33 @@ class LORA:
     def receive(self):
         BAUDRATE = 1000000
         RADIO_FREQ_MHZ = 915.0
-        CS = digitalio.DigitalInOut(board.CE1)
+
+        pinout = { # Pinout from the LoRaWAN RFM9x module to the Raspberry Pi
+            "3.3v": 1,
+            "GND": 6,
+            "DI00": 7,
+            "RESET": 11,
+            "NSS": 22,
+            "MOSI": 19,
+            "MISO": 21,
+            "SCK": 23,
+        }
+
+        CS = digitalio.DigitalInOut(board.D22)
         RESET = digitalio.DigitalInOut(board.D25)
         spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-    
+
+        # Create the RFM9x device
         rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=BAUDRATE)
+        
+        # Create a unique ID for the device
+        uid = adafruit_rfm9x.base_id
+        
+        # Print out the details of the RFM9x device we just created
+        print("\nRFM9x Device ID: {}".format(uid))
+        print("Antenna: {:.0f}°".format(rfm9x.tx_power))
+        print("\nWaiting for packets...")
+    
         rfm9x.tx_power = 23
 
         packet = rfm9x.receive()
