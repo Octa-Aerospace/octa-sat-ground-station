df25eb3fd5cd567e8cf177e8c93189fe62ff08c2
CxrlosKenobi <kenobi@ieee.org>
2022-01-21 18:10:47 -0300

    Update packet receiver class for gnd station

--- modules/houston.py
+++ modules/houston.py
@@ -1,20 +1,10 @@
 from time import sleep
-# from modules.mainModule import Buzzer
 from modules.transceiver import LORA
-from data.OctaCSV import OctaCSV as oc
 
 class Houston:
     def __init__(self):
         # self.Buzzer = Buzzer()
         self.LORA = LORA()
-        self.oc = oc()
-
-    # def Buzzer_beep(self):
-    #     self.Buzzer.beep_on()
-    #     sleep(0.5) #! if we put this sleep, that will affect the module's reads
-    #     self.Buzzer.beep_off() 
-    #     sleep(2) #! same the above
-    #     return '[ ok ] Successfully beeped'
 
     def LORA_receive(self):
         package = self.LORA.receive()
@@ -26,9 +16,5 @@ class Houston:
         return '[ ok ] Successfully saved'
 
     def start(self):
-        # self.Buzzer_beep() #* just beep
-        
         data = self.LORA_receive()
         print(data)
-
-        # self.black_box(file_name='../data/Houston.csv', data=data)
