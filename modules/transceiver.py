3f769a04c85eaab3911e2784c3e7264779cd91e3
CxrlosKenobi <kenobi@ieee.org>
2022-01-21 19:05:06 -0300

    Fix class

--- modules/transceiver.py
+++ modules/transceiver.py
@@ -23,17 +23,21 @@ class LoraRF:
 		rfm9x.tx_power = 23 # (5 - 23)dBm
 		
 		print('\n[ ok ] RFM95w Radio configured.\n')
-		while True:
-			packet = rfm9x.receive()
-			rssi = rfm9x.rssi # Signal strength of last received message
-			snr = rfm9x.snr # Signal to noise ratio
+		try: 
+			while True:
+				packet = rfm9x.receive()
+				rssi = rfm9x.rssi # Signal strength of last received message
+				snr = rfm9x.snr # Signal to noise ratio
 
-			if packet is None:
-				print('[ - ] Listening ...')
-			else:
-				print('\n[ ! ] Received packet:')
-				print('[ + ] Signal strength (RSSI): {0} dBm'.format(rssi))
-				print('[ + ] SNR: {0} dB'.format(snr))
-				print('[ + ] Data (raw bytes): {0}'.format(packet))
-				
-				return str(packet, 'utf-8')
+				if packet is None:
+					print('[ - ] Listening ...')
+				else:
+					print('\n[ ! ] Received packet:')
+					print('[ + ] Signal strength (RSSI): {0} dBm'.format(rssi))
+					print('[ + ] SNR: {0} dB'.format(snr))
+					print('[ + ] Data (raw bytes): {0}'.format(packet))
+					
+					return str(packet, 'utf-8')
+		except KeyboardInterrupt:
+			print('\n[ x ] Listening stopped.')
+			return None
